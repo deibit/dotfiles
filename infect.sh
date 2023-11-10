293a065df714d7de8fafa94bc3992a34aab7b1fb
David García <daganu@gmail.com>
2023-11-10 11:32:19 +0100

    added wezterm configuration

--- infect.sh
+++ infect.sh
@@ -9,3 +9,6 @@ ln -sfn "$REPO/starship.toml" ~/.config/starship.toml
 
 [ -d $HOME/.config/lazygit ] || mkdir -p $HOME/.config/lazygit
 ln -sfn "$REPO/lazygit.yml" ~/.config/lazygit/config.yml
+
+[ -d $HOME/.config/wezterm ] || mkdir -p $HOME/.config/wezterm
+ln -sfn "$REPO/wezterm.lua" ~/.config/wezterm/wezterm.lua
---
+++ wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 14
+config.color_scheme = "Tokyo Night"
+
+return config
