9e08c37fad049e0d41290b0367f510a293c770bd
David García <daganu@gmail.com>
2023-11-11 07:32:45 +0100

    ligatures off, added tmux tab autorenaming

--- wezterm.lua
+++ wezterm.lua
@@ -1,8 +1,9 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.font = wezterm.font("JetBrains Mono")
+config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
 config.font_size = 14
 config.color_scheme = "Tokyo Night"
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 return config
