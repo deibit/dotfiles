e48db6843702b66f2320ff7b30f6dc7cf2d1bf47
David García <daganu@gmail.com>
2024-05-05 19:14:25 +0200

    tidy up telescope conf

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,80 +4,65 @@ return {
 		"nvim-lua/plenary.nvim",
 		"nvim-telescope/telescope-ui-select.nvim",
 		"nvim-telescope/telescope-frecency.nvim",
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "make",
+		},
 	},
 	config = function()
-		require("telescope").setup({
-
-			dependencies = {
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
-				config = function()
-					require("telescope").load_extension("fzf")
-					require("telescope").load_extension("file_browser")
-					require("telescope").load_extension("ui-select")
-					require("telescope").load_extension("frecency")
-				end,
-			},
-
-			defaults = {
-				-- Default configuration for telescope goes here:
-				-- config_key = value,
-			},
-			pickers = {
-				-- Default configuration for builtin pickers goes here:
-				-- picker_name = {
-				--   picker_config_key = value,
-				--   ...
-				-- }
-				-- Now the picker_config_key will be applied every time you call this
-				-- builtin picker
-				find_files = {
-					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-				},
-			},
-			extensions = {
-				-- Your extension configuration goes here:
-				-- extension_name = {
-				--   extension_config_key = value,
-				-- }
-				-- please take a look at the readme of the extension you want to configure
-
-				file_browser = {
-					theme = "ivy",
-					-- disables netrw and use telescope-file-browser in its place
-					hijack_netrw = true,
-				},
-
-				["ui-select"] = {
-					require("telescope.themes").get_dropdown({
-						-- even more opts
-					}),
-
-					-- pseudo code / specification for writing custom displays, like the one
-					-- for "codeactions"
-					-- specific_opts = {
-					--   [kind] = {
-					--     make_indexed = function(items) -> indexed_items, width,
-					--     make_displayer = function(widths) -> displayer
-					--     make_display = function(displayer) -> function(e)
-					--     make_ordinal = function(e) -> string
-					--   },
-					--   -- for example to disable the custom builtin "codeactions" display
-					--      do the following
-					--   codeactions = false,
-					-- }
-				},
-
-				fzf = {
-					fuzzy = true, -- false will only do exact matching
-					override_generic_sorter = true, -- override the generic sorter
-					override_file_sorter = true, -- override the file sorter
-					case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-					-- the default case_mode is "smart_case"
-				},
-			},
-		})
+		require("telescope").load_extension("fzf")
+		require("telescope").load_extension("ui-select")
+		require("telescope").load_extension("frecency")
 	end,
+	opt = {
+		pickers = {
+			-- Default configuration for builtin pickers goes here:
+			-- picker_name = {
+			--   picker_config_key = value,
+			--   ...
+			-- }
+			-- Now the picker_config_key will be applied every time you call this
+			-- builtin picker
+			find_files = {
+				find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+			},
+		},
+		extensions = {
+			-- Your extension configuration goes here:
+			-- extension_name = {
+			--   extension_config_key = value,
+			-- }
+			-- please take a look at the readme of the extension you want to configure
+
+			["ui-select"] = {
+				require("telescope.themes").get_dropdown({
+					-- even more opts
+				}),
+
+				-- pseudo code / specification for writing custom displays, like the one
+				-- for "codeactions"
+				-- specific_opts = {
+				--   [kind] = {
+				--     make_indexed = function(items) -> indexed_items, width,
+				--     make_displayer = function(widths) -> displayer
+				--     make_display = function(displayer) -> function(e)
+				--     make_ordinal = function(e) -> string
+				--   },
+				--   -- for example to disable the custom builtin "codeactions" display
+				--      do the following
+				--   codeactions = false,
+				-- }
+			},
+
+			fzf = {
+				fuzzy = true, -- false will only do exact matching
+				override_generic_sorter = true, -- override the generic sorter
+				override_file_sorter = true, -- override the file sorter
+				case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+				-- the default case_mode is "smart_case"
+			},
+		},
+	},
 	lazy = false,
 	keys = {
 		-- finders
