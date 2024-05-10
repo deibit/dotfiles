9718f007106d33e14ec4a09b62753e44735062a0
David García <daganu@gmail.com>
2024-05-10 23:27:56 +0200

    clean mason conf

--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -9,23 +9,24 @@ return {
 			cpp = { "clangtidy", "cppcheck" },
 			javascript = { "eslint" },
 		}
-		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-			group = vim.api.nvim_create_augroup("lint", { clear = true }),
 
-			callback = function()
-				local file_ext = vim.fn.expand("%:e")
-				if file_ext == "py" then
-					lint.try_lint("pylint")
-				elseif file_ext == "md" then
-					lint.try_lint("vale")
-				elseif file_ext == "c" or file_ext == "cpp" or file_ext == "h" then
-					lint.try_lint("clangtidy")
-				elseif file_ext == "js" then
-					lint.try_lint("eslint")
-				else
-					lint.try_lint()
-				end
-			end,
-		})
+		Run_linter = function()
+			local file_ext = vim.fn.expand("%:e")
+			if file_ext == "py" then
+				print("Running pylint on " .. file_ext .. "file")
+				lint.try_lint("pylint")
+			elseif file_ext == "md" then
+				lint.try_lint("vale")
+			elseif file_ext == "c" or file_ext == "cpp" or file_ext == "h" then
+				lint.try_lint("clangtidy")
+			elseif file_ext == "js" then
+				lint.try_lint("eslint")
+			else
+				lint.try_lint()
+			end
+		end
 	end,
+	keys = {
+		{ "<leader>l", "<cmd>lua Run_linter()<cr>", desc = "Run linter" },
+	},
 }
