eab4fd3a3c9afae175badfa04ab8513e0fc06a88
David García <daganu@gmail.com>
2024-05-11 20:42:21 +0200

    modify gitsign conf

--- nvim/lua/plugins/visuals.lua
+++ nvim/lua/plugins/visuals.lua
@@ -1,7 +1,17 @@
 return {
-	{ "nvim-tree/nvim-web-devicons", config = true },
-	{
-		"lewis6991/gitsigns.nvim",
-		config = true,
-	},
+    { "nvim-tree/nvim-web-devicons", config = true },
+    {
+        "lewis6991/gitsigns.nvim",
+        config = true,
+        opts = {
+            signs = {
+                add = { text = "▎" },
+                change = { text = "▎" },
+                delete = { text = "" },
+                topdelete = { text = "" },
+                changedelete = { text = "▎" },
+                untracked = { text = "▎" },
+            },
+        },
+    },
 }
