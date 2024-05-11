568399869cc0e0a5a1264c8ba7271a3cd129df04
David García <daganu@gmail.com>
2024-05-11 20:36:06 +0200

    modify conform conf

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,39 +1,70 @@
+-- Based on https://github.com/scottmckendry/Windots/blob/main/nvim/lua/plugins/conform.lua
+
 local get_venv_command = function(command)
-	if vim.env.VIRTUAL_ENV then
-		return vim.env.VIRTUAL_ENV .. "/bin/" .. command
-	else
-		return command
-	end
+    if vim.env.VIRTUAL_ENV then
+        return vim.env.VIRTUAL_ENV .. "/bin/" .. command
+    else
+        return command
+    end
 end
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = "*",
-	callback = function(args)
-		require("conform").format({ bufnr = args.buf })
-	end,
-})
-
 return {
-	"stevearc/conform.nvim",
-	opts = {
-    formatters = {black = {command = get_venv_command(("black"))}},
-		formatters_by_ft = {
-			-- Conform will run multiple formatters sequentially
-			-- Use a sub-list to run only the first available formatter
-			c = { { "clang-format" } },
-			cpp = { { "clang-format" } },
-			css = { { "prettier" } },
-			html = { { "prettier" } },
-			javascript = { { "prettier" } },
-			json = { { "prettier" } },
-			lua = { "stylua" },
-			python = { "isort", "black" },
-			yaml = { { "yamlfmt" } },
-		},
-		format_on_save = {
-			-- These options will be passed to conform.format()
-			timeout_ms = 500,
-			lsp_fallback = true,
-		},
-	},
+    "stevearc/conform.nvim",
+    event = "BufReadPre",
+    config = function()
+        vim.g.disable_autoformat = false
+        require("conform").setup({
+            formatters_by_ft = {
+                c = { { "clang-format" } },
+                cpp = { { "clang-format" } },
+                css = { "prettier" },
+                go = { "goimports_reviser", "gofmt", "golines" },
+                html = { "prettier" },
+                javascript = { "prettier" },
+                json = { "prettier" },
+                lua = { "stylua" },
+                markdown = { "prettier" },
+                sh = { "shfmt" },
+                toml = { "taplo" },
+                yaml = { "yamlfmt" },
+                python = { "isort", "black" },
+            },
+
+            format_after_save = function()
+                if vim.g.disable_autoformat then
+                    return
+                else
+                    return { lsp_fallback = true }
+                end
+            end,
+
+            formatters = {
+                black = {
+                    command = get_venv_command("black"),
+                },
+                goimports_reviser = {
+                    command = "goimports-reviser",
+                    args = { "-output", "stdout", "$FILENAME" },
+                },
+            },
+        })
+
+        -- Override stylua's default indent type
+        require("conform").formatters.stylua = {
+            prepend_args = { "--indent-type", "Spaces" },
+        }
+
+        -- Override prettier's default indent type
+        require("conform").formatters.prettier = {
+            prepend_args = { "--tab-width", "4" },
+        }
+
+        -- Toggle format on save
+        vim.api.nvim_create_user_command("ConformToggle", function()
+            vim.g.disable_autoformat = not vim.g.disable_autoformat
+            print("Conform " .. (vim.g.disable_autoformat and "disabled" or "enabled"))
+        end, {
+            desc = "Toggle format on save",
+        })
+    end,
 }
