fe11447a95c43548f31998fef8c0c6076f6fb95f
David García <daganu@gmail.com>
2024-05-12 13:26:31 +0200

    neotest finally configured

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -1,20 +1,34 @@
 return {
     "nvim-neotest/neotest",
+    ft = { "go", "python", "typescript", "javascript" },
     dependencies = {
+        "nvim-neotest/neotest-go",
+        "nvim-neotest/neotest-python",
         "nvim-neotest/nvim-nio",
         "nvim-lua/plenary.nvim",
         "antoinemadec/FixCursorHold.nvim",
         "nvim-treesitter/nvim-treesitter",
-        "nvim-neotest/neotest-python",
     },
-    opts = {
-        adapters = {
-            ["neotest-python"] = {
-                -- Here you can specify the settings for the adapter, i.e.
-                runner = "pytest",
-                -- python = '.venv/bin/python',
-                pytest_discover_instances = true,
+    opts = function()
+        return {
+            -- your neotest config here
+            adapters = {
+                require("neotest-python"),
+                require("neotest-go"),
             },
-        },
-    },
+        }
+    end,
+    config = function(_, opts)
+        -- get neotest namespace (api call creates or returns namespace)
+        local neotest_ns = vim.api.nvim_create_namespace("neotest")
+        vim.diagnostic.config({
+            virtual_text = {
+                format = function(diagnostic)
+                    local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+                    return message
+                end,
+            },
+        }, neotest_ns)
+        require("neotest").setup(opts)
+    end,
 }
