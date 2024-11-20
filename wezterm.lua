42c8480741ff0b5e1562ea459260d211b9383ca9
David García <daganu@gmail.com>
2024-11-20 10:25:27 +0100

    added direnv

--- wezterm.lua
+++ wezterm.lua
@@ -1,9 +1,10 @@
 local wezterm = require("wezterm")
 local config = {}
 
-config.font = wezterm.font("JetBrains Mono", { weight = "DemiBold" })
-config.font_size = 14
-config.color_scheme = "Tokyo Night"
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font_size = 17
+config.color_scheme = "catppuccin-mocha"
+
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 return config
