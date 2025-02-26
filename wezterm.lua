fecc06523ab996ea455a616ab8db392e26f6d90b
David García <daganu@gmail.com>
2025-02-26 14:32:39 +0100

    themes

--- wezterm.lua
+++ wezterm.lua
@@ -4,6 +4,6 @@ local config = {}
 config.font = wezterm.font("IosevkaTerm Nerd Font Mono")
 -- config.font_size = 26
 config.font_size = 18
-config.color_scheme = "catppuccin-mocha"
+config.color_scheme = "Tomorrow Night Eighties"
 
 return config
