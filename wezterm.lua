b32b99d29e34351056a1fede0ef07efc3f210410
David García <daganu@gmail.com>
2025-03-05 11:31:32 +0100

    change colors

--- wezterm.lua
+++ wezterm.lua
@@ -4,6 +4,6 @@ local config = {}
 config.font = wezterm.font("IosevkaTerm Nerd Font Mono")
 -- config.font_size = 26
 config.font_size = 19
-config.color_scheme = "catppuccin-mocha"
+config.color_scheme = "tokyonight-storm"
 
 return config
