d664a0ba9b000e3b625f852ee2b8f4b4dbeca439
David García <daganu@gmail.com>
2025-04-07 22:32:37 +0200

    clean

--- infect.sh
+++ infect.sh
@@ -32,7 +32,6 @@ done
 echo "${INFO} ${BLUE}Creando enlaces simbólicos...${RESET}"
 ln -sfn "$SCRIPT_DIR/tmux.conf" "$HOME/.tmux.conf" && echo "$LINK $HOME/.tmux.conf $CHECK"
 ln -sfn "$SCRIPT_DIR/zshrc" "$HOME/.zshrc" && echo "$LINK $HOME/.zshrc $CHECK"
-ln -sfn "$SCRIPT_DIR/zprofile" "$HOME/.zprofile" && echo "$LINK $HOME/.zprofile $CHECK"
 ln -sfn "$SCRIPT_DIR/nvim" "$XDG_CONFIG_HOME/nvim" && echo "$LINK $XDG_CONFIG_HOME/nvim $CHECK"
 ln -sfn "$SCRIPT_DIR/gitconfig" "$HOME/.gitconfig" && echo "$LINK $HOME/.gitconfig $CHECK"
 ln -sfn "$SCRIPT_DIR/lazygit.yml" "$XDG_CONFIG_HOME/lazygit/config.yml" && echo "$LINK $XDG_CONFIG_HOME/lazygit/config.yml $CHECK"
--- zprofile
+++
@@ -1,3 +0,0 @@
-
-# Created by `pipx` on 2024-07-19 11:43:10
-export PATH="$PATH:/Users/davidgarcia/.local/bin"
