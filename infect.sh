5980adb9bfb0dfa8c1771a929bebb25d15e60d27
David García <daganu@gmail.com>
2025-04-16 20:03:26 +0200

    better tmux conf

--- infect.sh
+++ infect.sh
@@ -37,7 +37,7 @@ Linux)
 esac
 
 # Crear directorios
-for i in lazygit wezterm eza tmux direnv yazi; do
+for i in tmux lazygit wezterm eza tmux direnv yazi; do
   d="$XDG_CONFIG_HOME/$i"
   echo "${FOLDER} ${GREEN}Creando directorio:${RESET} $d"
   mkdir -p "$d"
@@ -46,13 +46,13 @@ done
 # Crear enlaces simbólicos fijos
 echo "${INFO} ${BLUE}Creando enlaces simbólicos comunes...${RESET}"
 ln -sfn "$SCRIPT_DIR/zshrc" "$HOME/.zshrc" && echo "$LINK $HOME/.zshrc $CHECK"
-ln -sfn "$SCRIPT_DIR/tmux.conf" "$HOME/.tmux.conf" && echo "$LINK $HOME/.tmux.conf $CHECK"
 ln -sfn "$SCRIPT_DIR/nvim" "$XDG_CONFIG_HOME/nvim" && echo "$LINK $XDG_CONFIG_HOME/nvim $CHECK"
 ln -sfn "$SCRIPT_DIR/gitconfig" "$HOME/.gitconfig" && echo "$LINK $HOME/.gitconfig $CHECK"
 ln -sfn "$SCRIPT_DIR/lazygit.yml" "$XDG_CONFIG_HOME/lazygit/config.yml" && echo "$LINK $XDG_CONFIG_HOME/lazygit/config.yml $CHECK"
 ln -sfn "$SCRIPT_DIR/direnvrc" "$XDG_CONFIG_HOME/direnv/direnvrc" && echo "$LINK $XDG_CONFIG_HOME/direnv/direnvrc $CHECK"
 ln -sfn "$SCRIPT_DIR/yazi/yazi.toml" "$XDG_CONFIG_HOME/yazi/yazi.toml" && echo "$LINK $XDG_CONFIG_HOME/yazi/yazi.toml $CHECK"
 ln -sfn "$SCRIPT_DIR/yazi/init.lua" "$XDG_CONFIG_HOME/yazi/init.lua" && echo "$LINK $XDG_CONFIG_HOME/yazi/init.lua $CHECK"
+ln -sfn "$SCRIPT_DIR/tmux/tmux.conf" "$XDG_CONFIG_HOME/tmux/tmux.conf" && echo "$LINK $XDG_CONFIG_HOME/tmux/tmux.conf $CHECK"
 
 # Crear enlaces simbólicos variables (zshenv, zprofile)
 echo "${INFO} ${BLUE}Creando enlaces según el sistema: $SUFFIX...${RESET}"
