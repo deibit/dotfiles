4a5d4572fa1c898bb48ce3ae5f1fe39db5090141
David García <daganu@gmail.com>
2025-11-24 15:18:41 +0100

    reform conform

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -10,57 +10,9 @@ end
 
 return {
     "stevearc/conform.nvim",
-    config = function()
-        vim.g.disable_autoformat = false
-        require("conform").setup({
-            formatters_by_ft = {
-                c = { "clang-format" },
-                cpp = { "clang-format" },
-                css = { "prettier" },
-                html = { "prettier" },
-                go = { "goimports", "gofmt" },
-                javascript = { "prettier" },
-                json = { "jq" },
-                lua = { "stylua" },
-                markdown = { "prettier" },
-                sh = { "shfmt" },
-                toml = { "taplo" },
-                yaml = { "yamlfmt" },
-                python = { "ruff_format", "isort" },
-                sql = { "sqlfmt" },
-                vue = { "prettier" },
-            },
-
-            format_after_save = function()
-                if vim.g.disable_autoformat then
-                    return
-                else
-                    return { lsp_fallback = true }
-                end
-            end,
-
-            formatters = {
-                black = {
-                    command = get_venv_command("black"),
-                },
-                goimports_reviser = {
-                    command = "goimports-reviser",
-                    args = { "-output", "stdout", "$FILENAME" },
-                },
-            },
-        })
-
-        -- Override stylua's default indent type
-        require("conform").formatters.stylua = {
-            prepend_args = { "--indent-type", "Spaces" },
-        }
-
-        -- Override prettier's default indent type
-        require("conform").formatters.prettier = {
-            prepend_args = { "--tab-width", "4" },
-        }
-
-        -- Toggle format on save
+    event = "BufWritePre",
+    init = function()
+        vim.g.autoformat = true
         vim.api.nvim_create_user_command("ConformToggle", function()
             vim.g.disable_autoformat = not vim.g.disable_autoformat
             print("Conform " .. (vim.g.disable_autoformat and "disabled" or "enabled"))
@@ -68,4 +20,48 @@ return {
             desc = "Toggle format on save",
         })
     end,
+    opts = {
+        formatters_by_ft = {
+            c = { name = "clangd", timeout_ms = 500, lsp_format = "prefer" },
+            cpp = { name = "clangd", timeout_ms = 500, lsp_format = "prefer" },
+            go = { name = "gopls", timeout_ms = 500, lsp_format = "prefer" },
+            html = { "prettier" },
+            javascript = { "prettier", timeout_ms = 500, lsp_format = "fallback" },
+            json = { "prettier", timeout_ms = 500, lsp_format = "fallback" },
+            lua = { "stylua" },
+            markdown = { "prettier" },
+            python = { "ruff_format", "isort" },
+            rust = { name = "rust_analyzer", timeout_ms = 500, lsp_format = "prefer" },
+            sh = { "shfmt" },
+            sql = { "sqlfmt" },
+            toml = { "taplo" },
+            typescript = { "prettier", timeout_ms = 500, lsp_format = "fallback" },
+            vue = { "prettier" },
+            yaml = { "yamlfmt" },
+        },
+
+        format_after_save = function()
+            if vim.g.disable_autoformat then
+                return
+            else
+                return { lsp_fallback = true }
+            end
+        end,
+
+        formatters = {
+            black = {
+                command = get_venv_command("black"),
+            },
+            goimports_reviser = {
+                command = "goimports-reviser",
+                args = { "-output", "stdout", "$FILENAME" },
+            },
+            stylua = {
+                prepend_args = { "--indent-type", "Spaces" },
+            },
+            prettier = {
+                prepend_args = { "--tab-width", "4" },
+            },
+        },
+    },
 }
