e2954cb6d558ee5488ddd9a067c74f76b75049eb
David García <daganu@gmail.com>
2025-12-17 12:44:10 +0100

    [colorschemes] keep unused colorschemes, but in disable state

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,34 +1,38 @@
 return -- Using Lazy
 {
-    -- "navarasu/onedark.nvim",
-    -- config = function()
-    --     require("onedark").setup({
-    --         style = "dark",
-    --         code_style = {
-    --             keywords = "bold",
-    --         },
-    --     })
-    --     require("onedark").load()
-    -- end,
+    {
+        "navarasu/onedark.nvim",
+        enabled = false,
+        config = function()
+            require("onedark").setup({
+                style = "dark",
+                code_style = {
+                    keywords = "bold",
+                },
+            })
+            require("onedark").load()
+        end,
+    },
 
-    -- "folke/tokyonight.nvim",
-    "rebelot/kanagawa.nvim",
-    config = function()
-        require("kanagawa").setup({
-            theme = "lotus",
-            colors = {
-                theme = {
-                    all = {
-                        ui = {
-                            bg_gutter = "none",
+    { "folke/tokyonight.nvim", enabled = false },
+    {
+        "rebelot/kanagawa.nvim",
+        priority = 1000,
+        lazy = false,
+        config = function()
+            require("kanagawa").setup({
+                theme = "lotus",
+                colors = {
+                    theme = {
+                        all = {
+                            ui = {
+                                bg_gutter = "none",
+                            },
                         },
                     },
                 },
-            },
-        })
-    end,
-    -- "catppuccin/nvim",
-
-    priority = 1000,
-    lazy = false,
+            })
+        end,
+    },
+    { "catppuccin/nvim", enabled = false },
 }
