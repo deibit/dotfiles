d8a1d91d3c49a015c25c7e1640f231bd5368e6b7
David García <daganu@gmail.com>
2026-01-18 20:35:47 +0100

    fix blink breaking change

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,6 +1,5 @@
 return {
     "saghen/blink.cmp",
-    dependencies = { "onsails/lspkind.nvim" },
     build = "cargo +nightly build --release",
     event = "InsertEnter",
     -- optional: provides snippets for the snippet source
@@ -92,39 +91,22 @@ return {
                         -- { "source_name" },
                     },
                     treesitter = { "lsp" },
-                    -- from https://main.cmp.saghen.dev/recipes#nvim-web-devicons-lspkin
-                    -- later, from: https://github.com/onsails/lspkind.nvim/issues/90#issuecomment-2547614143
                     components = {
                         kind_icon = {
-                            ellipsis = false,
                             text = function(ctx)
-                                local icon = ctx.kind_icon
-                                if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                                    local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-                                    if dev_icon then
-                                        icon = dev_icon
-                                    end
-                                else
-                                    icon = require("lspkind").symbolic(ctx.kind, {
-                                        mode = "symbol",
-                                    })
-                                end
-
-                                return icon .. ctx.icon_gap
+                                local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                                return kind_icon
                             end,
-
-                            -- Optionally, use the highlight groups from nvim-web-devicons
-                            -- You can also add the same function for `kind.highlight` if you want to
-                            -- keep the highlight groups in sync with the icons.
+                            -- (optional) use highlights from mini.icons
                             highlight = function(ctx)
-                                local hl = "BlinkCmpKind" .. ctx.kind
-                                    or require("blink.cmp.completion.windows.render.tailwind").get_hl(ctx)
-                                if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                                    local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-                                    if dev_icon then
-                                        hl = dev_hl
-                                    end
-                                end
+                                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                                return hl
+                            end,
+                        },
+                        kind = {
+                            -- (optional) use highlights from mini.icons
+                            highlight = function(ctx)
+                                local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
                                 return hl
                             end,
                         },
