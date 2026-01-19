f111a2aff4fe54ee2c4f5d1cf6fb9f00dda00a4f
David García <daganu@gmail.com>
2026-01-19 14:35:12 +0100

    added precise filetype to prevent yamlls from taken docker-compose files as its

--- nvim/lsp/docker_compose.lua
+++ nvim/lsp/docker_compose.lua
@@ -1,7 +1,9 @@
 return {
     name = "docker_compose",
     cmd = { "docker-compose-langserver", "--stdio" },
-    filetypes = { "yaml", "yml", "docker-compose" },
+    filetypes = { "yaml.docker-compose" },
+    root_markers = { "docker-compose.yaml", "docker-compose.yml", "compose.yaml", "compose.yml" },
+
     root_dir = vim.fs.root(0, { "docker-compose.yml", "docker-compose.yaml", ".git" }),
 
     settings = {
