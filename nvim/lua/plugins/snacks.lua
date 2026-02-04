9299728d0fb2acf2d7cb626a9a92979e44407317
David García <daganu@gmail.com>
2026-02-04 10:27:39 +0100

    manpager, bigfile linelength, read .env files in direnv act.

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -26,7 +26,7 @@ return {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
-        bigfile = { enabled = true },
+        bigfile = { enabled = true, line_length = 100000 },
         bufdelete = { enabled = true },
         indent = { enabled = true },
         gitbrowse = { enabled = true },
